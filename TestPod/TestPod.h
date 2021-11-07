8f8d62177246eca69918eae057cd4372df9142d9
Khaled Saad <47607058+khaledsaad93@users.noreply.github.com>
2021-11-07 14:06:59 +0200

    Initial Commit

---
+++ TestPod/TestPod.h
@@ -0,0 +1,18 @@
+//
+//  TestPod.h
+//  TestPod
+//
+//  Created by Khaled Mahmoud Saad on 07/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPod.
+FOUNDATION_EXPORT double TestPodVersionNumber;
+
+//! Project version string for TestPod.
+FOUNDATION_EXPORT const unsigned char TestPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPod/PublicHeader.h>
+
+
